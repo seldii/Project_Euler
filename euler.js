223fe19b46adc349ebe3040f47f98a237a42d859
seldii <seldaguzel@gmail.com>
2019-10-21 18:53:11 +0200

    new solutions

--- euler.js
+++ euler.js
@@ -93,7 +93,6 @@ const Euler = {
   //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
   smallestMultiple: () => {
-
     function gcd(a, b) {
       return b === 0 ? a : gcd(b, a % b); // Euclidean algorithm
     }
@@ -104,9 +103,84 @@ const Euler = {
     let result = 1;
     for (var i = 2; i <= 20; ++i) result = lcm(result, i);
     return result;
+  },
+
+  //Find the difference between the sum of the squares of
+  //the first one hundred natural numbers and the square of the sum.
+  /* sumSquareDiff: () => {
+    let numbers = 0;
+    let numbersSq = 0;
+    for (let i = 1; i <= 100; i++) {
+      numbersSq += i * i;
+      numbers += i;
+    }
+    console.log(numbers);
+
+    return numbers * numbers - numbersSq;
+  } */
+
+  solution: n => {
+    return (n * (n + 1) * (3 * n * n - n - 2)) / 12;
+  },
+
+  //10001st prime
+
+  primeNumber: () => {
+    function isPrime(num) {
+      for (var i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    let count = 2;
+    let n = 0;
+
+    //all primes except 2 and 3 have the form 6k±1
+    while (count < 10001) {
+      n += 6;
+
+      if (isPrime(n + 1)) {
+        count++;
+      }
+      if (isPrime(n - 1)) {
+        count++;
+      }
+    }
+    // Add one for the final prime being of the form 6k + 1
+    console.log(n + 1);
+  },
+
+  //Largest product in a series
+
+  largetsProduct: N => {
+    let start = 0;
+    let end;
+    let len = N.length;
+    let total;
+    let max = -Infinity;
+    while (start <= len - 13) {
+      end = start + 13;
+      let newString = N.slice(start, end);
+      total = 1;
+      for (let i = 0; i < 13; i++) {
+        total *= parseInt(newString[i], 10);
+      }
+      if (total > max) {
+        max = total;
+        total = 1;
+      }
+      start++;
+    }
+    return max;
   }
 };
 
-console.log(Euler.smallestMultiple());
+const number =
+  "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+
+console.log(Euler.largetsProduct(number));
 
 module.exports = Euler;
